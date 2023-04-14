9cb241df1b00ba94be4fd49c91e4bb08a886c482
Idriss mahjoubi <mahjoubi.idriss@gmail.com>
2023-04-14 14:31:10 +0200

    update links

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -4,14 +4,14 @@ export const Styles = createGlobalStyle`
 
     @font-face {
         font-family: "Motiva Sans Light";
-        src: url("/fonts/Motiva-Sans-Light.ttf") format("truetype");
+        src: url("systems.tn/fonts/Motiva-Sans-Light.ttf") format("truetype");
         font-style: normal;
         font-size: 22px;        
     }
 
     @font-face {
         font-family: "Motiva Sans Bold";
-        src: url("/fonts/Motiva-Sans-Bold.ttf") format("truetype");
+        src: url("systems.tn/fonts/Motiva-Sans-Bold.ttf") format("truetype");
         font-style: normal;
         font-size: 22px;        
     }
