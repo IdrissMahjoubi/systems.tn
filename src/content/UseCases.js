84b10362ec4777d08627739c72e43553a0b19f6d
Idriss mahjoubi <mahjoubi.idriss@gmail.com>
2023-06-12 10:37:16 +0100

    remove static messages

--- src/content/UseCases.js
+++ src/content/UseCases.js
@@ -5,34 +5,34 @@ import AERONAUTICS from "../assets/AERONAUTICS.svg";
 
 export const data = {
   title: "Use cases",
-  text: "This template is ready to use, so you don't need to change anything at a component level, unless you want to customize the default styling.",
+  text: "",
   section: [
     {
       route: "pac",
       title: "Physical access control",
       content:
-        "Ant Design is a React UI library that has a lot of easy-to-use components for building elegant UI.",
+        "",
       icon: PAC,
     },
     {
       route: "bac",
       title: "Border access control",
       content:
-        "Styled Components gives you total control over your components.",
+        "",
       icon: BAC,
     },
     {
       route: "it",
       title: "IT solutions",
       content:
-        "Ant Design is a React UI library that has a lot of easy-to-use components for building elegant UI.",
+        "",
       icon: IT,
     },
     {
       route: "aeronautics",
       title: "Aeronautics",
       content:
-        "Styled Components gives you total control over your components.",
+        "",
       icon: AERONAUTICS,
     },
   ],
